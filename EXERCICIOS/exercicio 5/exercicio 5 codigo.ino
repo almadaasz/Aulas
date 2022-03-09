64f6d00c680915210ad253fcff045ce304bf07c9
almadaasz <99339400+almadaasz@users.noreply.github.com>
2022-03-09 11:50:30 -0300

    Add files via upload

---
+++ EXERCICIOS/exercicio 5/exercicio 5 codigo.ino
@@ -0,0 +1,22 @@
+const int botao1 = 2;
+const int led1 = 4;
+
+int var1;
+
+  
+ void setup()
+ { 
+    pinMode(botao1, INPUT); 
+    pinMode(led1, OUTPUT);
+ }
+ 
+ void loop()
+ {
+    var1 = digitalRead(botao1);
+   	
+   if(var1){
+     digitalWrite(led1, LOW);
+   }else{
+     digitalWrite(led1, HIGH);
+}
+}
